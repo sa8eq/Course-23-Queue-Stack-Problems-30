f1946c9f82317758f8bd1c38b6e68f56edf79c58
sa8eq <sadeq8omar@gmail.com>
2026-06-10 02:34:12 +0300

    Problem Done...

---
+++ Problem 2 Convert Decimal to Binary/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class Program
+{
+    static string DecimalToBinary(int number)
+    {
+        if (number == 0) return "0";
+        Stack<int> stack = new Stack<int>();
+        while (number > 0)
+        {
+            stack.Push(number % 2);
+            number /= 2;
+        }
+        return string.Join("", stack);
+    }
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Enter A Number To Convert To Binary:");
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int decimalNumber) && decimalNumber >= 0)
+            {
+                Console.WriteLine($"You entered: {decimalNumber}");
+                string binaryResult = DecimalToBinary(decimalNumber);
+                Console.WriteLine($"The Binary of {decimalNumber} After Converting is: {binaryResult}");
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid input! Please enter a valid positive integer.");
+            }
+        }
+    }
+}
+
